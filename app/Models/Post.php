bcaf5a9f10835a573710cfc37d09c21747c66662
Lychtael <jitabilhaq12@gmail.com>
2024-11-03 23:33:33 +0700

    Video 9 - Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,33 +1,12 @@
-<?php 
+<?php
+
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post {
-    public static function all() {
-        return [
-            [
-                'id'=>1,
-                'slug' => 'judul-artikel-1',
-                'title'=> 'Judul Artikel 1',
-                'author'=> 'jita',
-                'body'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus perspiciatis, autem vero reprehenderit sed maiores ullam vel odio, eos cumque expedita odit repellat itaque aliquam illo, suscipit voluptate illum. Inventore?',
-            ],
-            [
-                'id'=> 2,
-                'slug'=> 'judul-artikle-2',
-                'title'=> 'Judul Artikel 2',
-                'author'=> 'jita',
-                'body'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus perspiciatis, autem vero reprehenderit sed maiores ullam vel odio, eos cumque expedita odit repellat itaque aliquam illo, suscipit voluptate illum. Inventore?',
-            ]
-        ];
-    }
-
-    public static function find($slug): array {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-        if (! $post) {
-            abort(404);
-        }
-        return $post;
-    }
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title','author','slug','body'];
 }
