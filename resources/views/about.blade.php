87f7c25d5f6eb65c07a0077fca56fcd9ea5eb3c1
Lychtael <jitabilhaq12@gmail.com>
2024-11-03 12:38:42 +0700

    Video 5 - Blade Component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">about</a>
-    <h1>ABOUT</h1>
-    <img src="img/ffxiv.png" alt="" srcset="">
-    <h3>owner : {{ $nama }} </h3>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3>About</h3>
+    <p>nama sy : {{ $nama }}</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' =>'HomePage']);
 });
 Route::get('/about', function () {
-    return view('about', ['nama' =>'jita']);
+    return view('about', ['nama' =>'jita'],['title' =>'About']);
 });
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' =>'Blog']);
 });
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' =>'Contact']);
 });
