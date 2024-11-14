39b9eb0e08abb5c4a4a2732a838d828a303a675c
Lychtael <jitabilhaq12@gmail.com>
2024-11-14 21:28:22 +0700

    Video 16 - Searching

--- routes/web.php
+++ routes/web.php
@@ -15,29 +15,23 @@ Route::get('/about', function () {
     return view('about', ['nama' =>'jita'],['title' =>'About']);
 });
 Route::get('/posts', function () {
-    // $posts = Post::with(['author','category'])->latest()->get();
-    $posts = Post::latest()->get();
-    return view('posts', ['title' =>'Blog','posts'=>$posts]);
+    return view('posts', ['title' =>'Blog','posts'=>Post::filter(request(['search','category','author']))->latest()->get()]);
 });
-
 Route::get('/posts/{post:slug}', function(Post $post) {
 
     return view('post', ['title'=> 'Single Post','post'=> $post]);
 });
-
 Route::get('/authors/{user:username}', function(User $user) {
     // $posts = $user->posts->load('category','author');
 
     return view('posts', ['title'=> count($user->posts).' Articles by ' . $user->name,'posts'=> $user->posts]);
 });
-
 Route::get('/categories/{category:slug}', function(Category $category) {
     // $posts = $category->posts->load('category','author');
 
 
     return view('posts', ['title'=> 'Articles in Category: ' . $category->name,'posts'=> $category->posts]);
 });
-
 Route::get('/contact', function () {
     return view('contact', ['title' =>'Contact']);
 });
